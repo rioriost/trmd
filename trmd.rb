16bd50732d938c022bf0c7fb81559489347bd8dc
Rio Fujita <rio_github@rio.st>
2026-03-06 22:03:10 +0900

    Dependency update

--- trmd.rb
+++ trmd.rb
@@ -3,8 +3,8 @@ class Trmd < Formula
 
   desc "Translate Markdown using OpenAI"
   homepage "https://github.com/rioriost/homebrew-trmd/"
-  url "https://files.pythonhosted.org/packages/eb/c9/a94103d09a9ffd995acf9e1893cb825f8bd530c8a65faeb36d33bcfe01f8/trmd-0.1.34.tar.gz"
-  sha256 "1305724cbb944585566deb234da80fb61699da9eba311acbf883fc4dd39a9193"
+  url "https://files.pythonhosted.org/packages/cd/f0/8aaf49f4749e46db498e6151f9778171c066f56d6b9e62920793a467e3c5/trmd-0.1.35.tar.gz"
+  sha256 "a4ad7225eb9188e7479b63f7a54ae3cc4edd202a134896a376d9d67f1a93bf11"
   license "MIT"
 
   depends_on "python@3.13"
